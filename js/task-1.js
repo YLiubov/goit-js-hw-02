71e578bcabc28082040b1bdbeccb852335668000
YLiubov <129996599+YLiubov@users.noreply.github.com>
2024-01-11 00:33:15 +0100

    1

--- js/task-1.js
+++ js/task-1.js
@@ -9,3 +9,12 @@ console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000)); 
 console.log(makeTransaction(10, 500)); 
 
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
+  
+    if (totalPrice > customerCredits) {
+      return "Insufficient funds!";
+    } else {
+      return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+  }
